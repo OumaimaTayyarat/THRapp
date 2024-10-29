5310c09f534ccd40e171d9ac3048e77ecd75ea90
Oumaima Tayyarat <158475593+OumaimaTayyarat@users.noreply.github.com>
2024-10-29 22:59:48 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ const corsOptions = {
         }
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["get", "post", "put", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     optionsSuccessStatus: 200
 };
