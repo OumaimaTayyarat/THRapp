b8304586cdc1782eed7cadc9af1bf3bf0ae3531e
Oumaima Tayyarat <158475593+OumaimaTayyarat@users.noreply.github.com>
2024-10-29 21:11:41 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:"https://thr-app.vercel.app/",
+    origin:process.env.LOCALHOST,
    
     credentials:true
 }
