d378b3b01f7d84f3413530bf6905d9b56d96f064
Oumaima Tayyarat <158475593+OumaimaTayyarat@users.noreply.github.com>
2024-10-31 21:01:28 +0100

    Update Signup.jsx

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from 'react'
 import Navbar from '../shared/Navbar'
 import { Label } from '../ui/label'
@@ -16,7 +15,6 @@ import Navbar1 from '../shared/Navbar1'
 import Footer from '../shared/Footer'
 
 const Signup = () => {
-
     const [input, setInput] = useState({
         fullname: "",
         email: "",
@@ -25,38 +23,41 @@ const Signup = () => {
         role: "",
         file: ""
     });
-    const {loading,user} = useSelector(store=>store.auth);
+
+    const {loading, user} = useSelector(store => store.auth);
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
     const changeEventHandler = (e) => {
         setInput({ ...input, [e.target.name]: e.target.value });
     }
+
     const changeFileHandler = (e) => {
         setInput({ ...input, file: e.target.files?.[0] });
     }
+
     const submitHandler = async (e) => {
         e.preventDefault();
-        const formData = new FormData();    //formdata object
+
+        // Vérifier si le fichier est absent et afficher un toast d'erreur
+        if (!input.file) {
+            toast.error("Please upload a profile photo.");
+            return;  // Empêcher la soumission si le fichier n'est pas présent
+        }
+
+        const formData = new FormData();
         formData.append("fullname", input.fullname);
         formData.append("email", input.email);
         formData.append("phoneNumber", input.phoneNumber);
         formData.append("password", input.password);
         formData.append("role", input.role);
-        if (input.file) {
-            formData.append("file", input.file);
-  
-        }
-                  console.log(`${USER_API_END_POINT}/register`)
+        formData.append("file", input.file);
 
         try {
             dispatch(setLoading(true));
-            const res = await axios.post("https://thr-backend.onrender.com/api/v1/user/register", formData, {
+            const res = await axios.post(`${USER_API_END_POINT}/register`, formData, {
                 headers: { 'Content-Type': "multipart/form-data" },
                 withCredentials: true,
-
-             
-                
             });
             if (res.data.success) {
                 navigate("/login");
@@ -65,129 +66,126 @@ const Signup = () => {
         } catch (error) {
             console.log(error);
             toast.error(error.response.data.message);
-        } finally{
+        } finally {
             dispatch(setLoading(false));
         }
     }
 
-    useEffect(()=>{
-        if(user){
+    useEffect(() => {
+        if (user) {
             navigate("/");
         }
-    },[])
+    }, []);
+
     return (
         <div>
-            <Navbar1/>
+            <Navbar1 />
             <br />
             <br />
             <br />
             <br />
             <div className='flex items-center justify-center max-w-7xl mx-auto'>
-            <form onSubmit={submitHandler} className='w-1/2 border border-gray-200 rounded-md p-4 my-10'>
-  <h1 className='font-bold text-xl mb-5' style={{ color: '#7f99b5' }}>Sign Up</h1>
-  
-  <div className='my-2'>
-    <Label style={{ color: '#7f99b5' }}>Full Name</Label>
-    <Input
-      type="text"
-      value={input.fullname}
-      name="fullname"
-      onChange={changeEventHandler}
-      placeholder="patel"
-    />
-  </div>
-  
-  <div className='my-2'>
-    <Label style={{ color: '#7f99b5' }}>Email</Label>
-    <Input
-      type="email"
-      value={input.email}
-      name="email"
-      onChange={changeEventHandler}
-      placeholder="patel@gmail.com"
-    />
-  </div>
-  
-  <div className='my-2'>
-    <Label style={{ color: '#7f99b5' }}>Phone Number</Label>
-    <Input
-      type="text"
-      value={input.phoneNumber}
-      name="phoneNumber"
-      onChange={changeEventHandler}
-      placeholder="8080808080"
-    />
-  </div>
-  
-  <div className='my-2'>
-    <Label style={{ color: '#7f99b5' }}>Password</Label>
-    <Input
-      type="password"
-      value={input.password}
-      name="password"
-      onChange={changeEventHandler}
-      placeholder="******************"
-    />
-  </div>
-  
-  <div className='flex items-center justify-between'>
-    <RadioGroup className="flex items-center gap-4 my-5">
-      <div className="flex items-center space-x-2">
-        <Input
-          type="radio"
-          name="role"
-          value="student"
-          checked={input.role === 'student'}
-          onChange={changeEventHandler}
-          className="cursor-pointer"
-        />
-        <Label htmlFor="r1" style={{ color: '#7f99b5' }}>Candidate</Label>
-      </div>
-      <div className="flex items-center space-x-2">
-        <Input
-          type="radio"
-          name="role"
-          value="recruiter"
-          checked={input.role === 'recruiter'}
-          onChange={changeEventHandler}
-          className="cursor-pointer"
-        />
-        <Label htmlFor="r2" style={{ color: '#7f99b5' }}>Recruiter</Label>
-      </div>
-    </RadioGroup>
-    
-    <div className='flex items-center gap-2'>
-  <Label style={{ color: '#7f99b5', marginLeft: '18px' }}>Profile</Label>
-  <Input
-    accept="image/*"
-    type="file"
-    onChange={changeFileHandler}
-    className="cursor-pointer"
-    aria-label="Choose a file" // Accessible label
-  />
-</div>
+                <form onSubmit={submitHandler} className='w-1/2 border border-gray-200 rounded-md p-4 my-10'>
+                    <h1 className='font-bold text-xl mb-5' style={{ color: '#7f99b5' }}>Sign Up</h1>
 
-  </div>
-  
-  {
-    loading ? (
-      <Button className="w-full my-4">
-        <Loader2 className='mr-2 h-4 w-4 animate-spin' /> Please wait
-      </Button>
-    ) : (
-      <Button type="submit" className="w-full my-4 border-[#edb526] text-[#edb526] hover:bg-[#edb526] hover:text-white">Signup</Button>
-    )
-  }
-  
-  <p className='text-sm' style={{ color: '#7f99b5' }}>
-    Already have an account? <Link to="/login" className='text-blue-600'>Login</Link>
-  </p>
-</form>
+                    <div className='my-2'>
+                        <Label style={{ color: '#7f99b5' }}>Full Name</Label>
+                        <Input
+                            type="text"
+                            value={input.fullname}
+                            name="fullname"
+                            onChange={changeEventHandler}
+                            placeholder="patel"
+                        />
+                    </div>
 
+                    <div className='my-2'>
+                        <Label style={{ color: '#7f99b5' }}>Email</Label>
+                        <Input
+                            type="email"
+                            value={input.email}
+                            name="email"
+                            onChange={changeEventHandler}
+                            placeholder="patel@gmail.com"
+                        />
+                    </div>
+
+                    <div className='my-2'>
+                        <Label style={{ color: '#7f99b5' }}>Phone Number</Label>
+                        <Input
+                            type="text"
+                            value={input.phoneNumber}
+                            name="phoneNumber"
+                            onChange={changeEventHandler}
+                            placeholder="8080808080"
+                        />
+                    </div>
+
+                    <div className='my-2'>
+                        <Label style={{ color: '#7f99b5' }}>Password</Label>
+                        <Input
+                            type="password"
+                            value={input.password}
+                            name="password"
+                            onChange={changeEventHandler}
+                            placeholder="***********"
+                        />
+                    </div>
+
+                    <div className='flex items-center justify-between'>
+                        <RadioGroup className="flex items-center gap-4 my-5">
+                            <div className="flex items-center space-x-2">
+                                <Input
+                                    type="radio"
+                                    name="role"
+                                    value="student"
+                                    checked={input.role === 'student'}
+                                    onChange={changeEventHandler}
+                                    className="cursor-pointer"
+                                />
+                                <Label htmlFor="r1" style={{ color: '#7f99b5' }}>Candidate</Label>
+                            </div>
+                            <div className="flex items-center space-x-2">
+                                <Input
+                                    type="radio"
+                                    name="role"
+                                    value="recruiter"
+                                    checked={input.role === 'recruiter'}
+                                    onChange={changeEventHandler}
+                                    className="cursor-pointer"
+                                />
+                                <Label htmlFor="r2" style={{ color: '#7f99b5' }}>Recruiter</Label>
+                            </div>
+                        </RadioGroup>
+
+                        <div className='flex items-center gap-2'>
+                            <Label style={{ color: '#7f99b5', marginLeft: '18px' }}>Profile</Label>
+                            <Input
+                                accept="image/*"
+                                type="file"
+                                onChange={changeFileHandler}
+                                className="cursor-pointer"
+                                aria-label="Choose a file" // Accessible label
+                            />
+                        </div>
+                    </div>
+
+                    {loading ? (
+                        <Button className="w-full my-4">
+                            <Loader2 className='mr-2 h-4 w-4 animate-spin' /> Please wait
+                        </Button>
+                    ) : (
+                        <Button type="submit" className="w-full my-4 border-[#edb526] text-[#edb526] hover:bg-[#edb526] hover:text-white">Signup</Button>
+                    )}
+
+                    <p className='text-sm' style={{ color: '#7f99b5' }}>
+                        Already have an account? <Link to="/login" className='text-blue-600'>Login</Link>
+                    </p>
+                </form>
             </div>
-            <Footer/>
+            <Footer />
         </div>
-    )
+    );
 }
 
-export default Signup
+export default Signup;
