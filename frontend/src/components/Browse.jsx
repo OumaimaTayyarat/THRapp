edff67df1c4c64aa4bd842b79fceca98b5fa5658
Oumaima Tayyarat <158475593+OumaimaTayyarat@users.noreply.github.com>
2024-10-31 17:01:14 +0100

    Update Browse.jsx

--- frontend/src/components/Browse.jsx
+++ frontend/src/components/Browse.jsx
@@ -1,28 +1,33 @@
-import React, { useEffect } from 'react'
-import Navbar from './shared/Navbar'
+import React, { useEffect, useState } from 'react';
+import Navbar from './shared/Navbar';
 import Job from './Job';
 import { useDispatch, useSelector } from 'react-redux';
 import { setSearchedQuery } from '@/redux/jobSlice';
 import useGetAllJobs from '@/hooks/useGetAllJobs';
 import Navbar1 from './shared/Navbar1';
 import Footer from './shared/Footer';
-import "./Home.css"
-
-// const randomJobs = [1, 2,45];
+import './Home.css';
 
 const Browse = () => {
     const dispatch = useDispatch();
-    useGetAllJobs()
+    const [isLoading, setIsLoading] = useState(true); // Indicateur de chargement
     const { allJobs } = useSelector(store => store.job);
+    dispatch(setSearchedQuery(""));
+    // Utiliser le hook pour récupérer les jobs
+    useGetAllJobs();
 
     useEffect(() => {
         // Réinitialiser `searchedQuery` au montage de `Browse`
         dispatch(setSearchedQuery(""));
-    }, [dispatch]);
+
+        // Vérifier si `allJobs` a bien des éléments une fois récupéré
+        if (allJobs.length > 0) {
+            setIsLoading(false); // Arrêter le chargement une fois les jobs disponibles
+        }
+    }, [allJobs, dispatch]);
+
     return (
         <div className='body'>
-            <br />
-
             <br />
             <br />
             <br />
@@ -43,20 +48,21 @@ const Browse = () => {
 
             <div className='max-w-7xl mx-auto my-10'>
                 <h1 className='font-bold text-xl my-10'></h1>
-                <div className='grid grid-cols-3 gap-4'>
-                    {
-                        allJobs.map((job) => {
-                            return (
-                                <Job key={job._id} job={job} />
-                            )
-                        })
-                    }
-                </div>
-
+                
+                {isLoading ? (
+                    <p>Loading jobs...</p>  // Indicateur de chargement
+                ) : (
+                    <div className='grid grid-cols-3 gap-4'>
+                        {allJobs.map((job) => (
+                            <Job key={job._id} job={job} />
+                        ))}
+                    </div>
+                )}
             </div>
+
             <Footer />
         </div>
-    )
+    );
 }
 
-export default Browse
+export default Browse;
