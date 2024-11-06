50efbcd0eaae30a39e6fa688d6771a660f66286f
Oumaima Tayyarat <158475593+OumaimaTayyarat@users.noreply.github.com>
2024-11-06 14:27:46 +0100

    Update isAuthenticated.js

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -1,3 +1,5 @@
+import jwt from "jsonwebtoken";
+
 const isAuthenticated = async (req, res, next) => {
     try {
         const token = req.cookies.token;
@@ -5,22 +7,19 @@ const isAuthenticated = async (req, res, next) => {
             return res.status(401).json({
                 message: "User not authenticated",
                 success: false,
-            });
+            })
         }
         const decode = await jwt.verify(token, process.env.SECRET_KEY);
-        if (!decode) {
+        if(!decode){
             return res.status(401).json({
-                message: "Invalid token",
-                success: false
-            });
-        }
+                message:"Invalid token",
+                success:false
+            })
+        };
         req.id = decode.userId;
         next();
     } catch (error) {
         console.log(error);
-        return res.status(500).json({
-            message: "Server error",
-            success: false,
-        });
     }
 }
+export default isAuthenticated;
