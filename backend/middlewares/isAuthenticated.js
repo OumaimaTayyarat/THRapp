28e44db979b819201a2396a70b7b34d5594b3611
Oumaima Tayyarat <158475593+OumaimaTayyarat@users.noreply.github.com>
2024-11-06 14:20:00 +0100

    Update isAuthenticated.js

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -1,6 +1,6 @@
 const isAuthenticated = async (req, res, next) => {
     try {
-        const token = req.headers['authorization']?.split(' ')[1]; // Récupère le token dans l'en-tête Authorization
+        const token = req.cookies.token;
         if (!token) {
             return res.status(401).json({
                 message: "User not authenticated",
