30b1926423d22d534dcba6de9b0a6465ddc81010
Oumaima Tayyarat <158475593+OumaimaTayyarat@users.noreply.github.com>
2024-11-07 16:35:06 +0100

    Update useGetJobById.jsx

--- frontend/src/hooks/useGetJobById.jsx
+++ frontend/src/hooks/useGetJobById.jsx
@@ -6,12 +6,17 @@ import { useDispatch } from 'react-redux';
 
 const useGetJobById = (jobId) => {
     const dispatch = useDispatch();
+const token = localStorage.getItem('token');  // Retrieve the token from localStorage
 
     useEffect(() => {
         const fetchSingleJob = async () => {
             try {
-                const res = await axios.get(`${JOB_API_END_POINT}/get/${jobId}`, { withCredentials: true });
-                console.log(res.data.job);
+const res = await axios.get(`${JOB_API_END_POINT}/get/${jobId}`, {
+  headers: {
+    Authorization: `Bearer ${token}`,  // Add the token in the Authorization header
+  },
+  withCredentials: true,  // Include cookies if needed
+});                console.log(res.data.job);
                 
                 if (res.data.success) {
                     dispatch(setSingleJob(res.data.job));  // Dispatch job to Redux store
