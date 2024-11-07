be3124f000586c325c995aee2310c22ecf653a3e
Oumaima Tayyarat <158475593+OumaimaTayyarat@users.noreply.github.com>
2024-11-07 16:57:01 +0100

    Update useGetAppliedJobs.jsx

--- frontend/src/hooks/useGetAppliedJobs.jsx
+++ frontend/src/hooks/useGetAppliedJobs.jsx
@@ -1,31 +1,33 @@
 import { setAllAppliedJobs } from "@/redux/jobSlice";
 import { APPLICATION_API_END_POINT } from "@/utils/constant";
-import axios from "axios"
-import { useEffect } from "react"
-import { useDispatch } from "react-redux"
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 const useGetAppliedJobs = () => {
-    const dispatch = useDispatch();
-const token = localStorage.getItem('token');  // Retrieve token from localStorage
+  const dispatch = useDispatch();
+  const token = localStorage.getItem("token");
 
-    useEffect(()=>{
-        const fetchAppliedJobs = async () => {
-            try {
-const res = await axios.get(`${APPLICATION_API_END_POINT}/get`, {
-  headers: {
-    Authorization: `Bearer ${token}`,  // Include token in Authorization header
-  },
-  withCredentials: true,
-})                console.log(res.data)
+  useEffect(() => {
+    const fetchAppliedJobs = async () => {
+      try {
+        const res = await axios.get(`${APPLICATION_API_END_POINT}/get`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          withCredentials: true,
+        });
+        console.log(res.data);
 
-                if(res.data.success){
-                    dispatch(setAllAppliedJobs(res.data.application));
-                }
-            } catch (error) {
-                console.log(error);
-            }
+        if (res.data.success) {
+          dispatch(setAllAppliedJobs(res.data.application));
         }
-        fetchAppliedJobs();
-    },[])
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchAppliedJobs();
+  }, []);
 };
+
 export default useGetAppliedJobs;
