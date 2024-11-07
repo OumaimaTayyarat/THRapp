5e7347be7c7cd4d5e69cc1f2683d87d6f94b8fd8
Oumaima Tayyarat <158475593+OumaimaTayyarat@users.noreply.github.com>
2024-11-07 21:18:42 +0100

    Update datauri.js

--- backend/utils/datauri.js
+++ backend/utils/datauri.js
@@ -1,11 +1,15 @@
-import DataUriParser from "datauri/parser.js"
-
+import DataUriParser from "datauri/parser.js";
 import path from "path";
 
 const getDataUri = (file) => {
+    // Check if the file is undefined or null before processing
+    if (!file || !file.buffer) {
+        throw new Error("No file provided or file is missing required properties.");
+    }
+
     const parser = new DataUriParser();
-    const extName = path.extname(file.originalname).toString();
+    const extName = path.extname(file.originalname).toString(); // Get file extension
     return parser.format(extName, file.buffer);
-}
+};
 
 export default getDataUri;
