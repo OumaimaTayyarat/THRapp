41e832d60864512a230d62575686abb7d35f6547
Oumaima Tayyarat <158475593+OumaimaTayyarat@users.noreply.github.com>
2024-11-07 16:34:12 +0100

    Update useGetCompanyById.jsx

--- frontend/src/hooks/useGetCompanyById.jsx
+++ frontend/src/hooks/useGetCompanyById.jsx
@@ -7,11 +7,17 @@ import { useDispatch } from 'react-redux'
 
 const useGetCompanyById = (companyId) => {
     const dispatch = useDispatch();
+    const token = localStorage.getItem('token');  // Retrieve token from localStorage
+
     useEffect(()=>{
         const fetchSingleCompany = async () => {
             try {
-                const res = await axios.get(`${COMPANY_API_END_POINT}/get/${companyId}`,{withCredentials:true});
-                console.log(res.data.company);
+const res = await axios.get(`${COMPANY_API_END_POINT}/get/${companyId}`, {
+  headers: {
+    Authorization: `Bearer ${token}`,  // Include the token in the Authorization header
+  },
+  withCredentials: true,
+});                console.log(res.data.company);
                 if(res.data.success){
                     dispatch(setSingleCompany(res.data.company));
                 }
