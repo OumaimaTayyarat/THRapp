2db311df9a0db9738e6cb87f9bb1608c026af2e2
Oumaima Tayyarat <158475593+OumaimaTayyarat@users.noreply.github.com>
2024-11-08 09:14:48 +0100

    Update user.model.js

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -25,7 +25,7 @@ const userSchema = new mongoose.Schema({
     },
     profile:{
         bio:{type:String},
-        skills:[{type:String}],
+        Establishment:[{type:String}],
         resume:{type:String}, // URL to resume file
         resumeOriginalName:{type:String},
         company:{type:mongoose.Schema.Types.ObjectId, ref:'Company'}, 
